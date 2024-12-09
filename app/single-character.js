6ff6364bdafc69c871a8f15082b1605b323669a9
emily-thieu56 <152931248+emily-thieu56@users.noreply.github.com>
2024-12-09 10:47:27 -0700

    Update single-character

--- app/single-character.js
+++ app/single-character.js
@@ -3,31 +3,28 @@
 import { useEffect, useState } from "react";
 import DisneyCharacter from "./disney-character";
 
-export default function SingleCharacter(){
-    const [singleCharacter, setSingleCharacter] = useState(null);
+export default function SingleCharacter() {
+  const [singleCharacter, setSingleCharacter] = useState(null);
 
-    async function getSingleCharacter() {
-        try {
-            const response = await fetch(
-                "https://api.disneyapi.dev/character"
-            );
-            //console.dir(response);
-            if ( !response.ok ) console.log(response.statusText);
-            const data = await response.json();
-            //console.dir(data);
-            setSingleCharacter(data);
-        } catch(error) {
-            console.log( `Error: ${error.message}`);
-        } 
-
-        useEffect(() => {
-            getSingleCharacter();
-           }, []);
-
-           return (
-            <section>
-                {singleCharacter && <DisneyCharacter characterObj={singleCharacter}/>}
-            </section>
-        );
+  async function getSingleCharacter() {
+    try {
+      const response = await fetch("https://api.disneyapi.dev/character");
+      //console.dir(response);
+      if (!response.ok) console.log(response.statusText);
+      const data = await response.json();
+      //console.dir(data);
+      setSingleCharacter(data);
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
     }
+  }
+  useEffect(() => {
+    getSingleCharacter();
+  }, []);
+
+  return (
+    <section>
+      {singleCharacter && <DisneyCharacter characterObj={singleCharacter} />}
+    </section>
+  );
 }
